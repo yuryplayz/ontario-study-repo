42e5b5fe1c8c552c1ee6fde41246109e58ca794b
Yury Vlasenko <yury.aws@gmail.com>
2025-12-15 14:42:49 -0500

    Suppress hydration warnings caused by browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-50 text-zinc-900`}
       >
         <SiteHeader />
